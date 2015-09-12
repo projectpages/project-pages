2c5844039696fdde183e1fbeb5f86ab31928622b
Ahmet Cecen <ahmetcecen@gmail.com>
2015-09-12 18:34:18 -0400

    Update AutoDeploy

--- PPInit.bat
+++ PPInit.bat
@@ -13,6 +13,10 @@
 @set PLINK_PROTOCOL=ssh
 @if not defined TERM set TERM=msys
 
+:: Install Curl
+@curl-7.43.0-win64.msi
+@del "curl-7.43.0-win64.msi"
+
 :: Get Parameters for Repo Initialization
 @echo Create GitHub Repository 
 @set /p User=User:%=%
